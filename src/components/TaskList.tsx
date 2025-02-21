4e3d47b7a9174093ad85217065caad13825d1caa
VighneshVejandla2003 <vighnesh.2003vejandla@gmail.com>
2025-02-21 22:13:14 +0530

    Changes

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,7 +4,8 @@ import React from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import "./home.css";
-import { tasks, Task } from "@/lib/tasks"; // Import from the new file
+import { tasks, Task } from "@/lib/tasks"; 
+import { TooltipItem } from 'chart.js';
 
 // Register Chart.js components
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -61,7 +62,7 @@ const Home: React.FC = () => {
       },
       tooltip: {
         callbacks: {
-          label: (context: any) => `${context.label}: ${context.raw} tasks`,
+          label: (context: TooltipItem<"doughnut">) => `${context.label}: ${context.raw} tasks`,
         },
       },
     },
